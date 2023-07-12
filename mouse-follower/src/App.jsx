f9f0e1600741fddba03a8a629af1212017d4f4e7
Jose Armas <armasjose606@gmail.com>
2023-07-12 13:32:41 -0400

    Add: mouse follower effect

--- mouse-follower/src/App.jsx
+++ mouse-follower/src/App.jsx
@@ -1,21 +1,48 @@
 import { useState, useEffect } from 'react'
-import './App.css'
 
 function App() {
 
   const [enabled, setEnabled] = useState(false)
+  const [position, setPosition] = useState({x:0, y:0})
 
   useEffect(() => {
-    console.log('effect', {enabled})
+    console.log('effect', { enabled })
+    
+    const handleMove = (event) => {
+      const { clientX, clientY } = event
+      console.log('handleMove', { clientX, clientY })
+      setPosition({x: clientX, y:clientY})
+    }
+
+    if (enabled) {
+      window.addEventListener('pointermove', handleMove)
+    }
+
+    return () => {
+      window.removeEventListener('pointermove', handleMove)
+    }
+
   }, [enabled])
 
-  return ( 
+  return (
     <>
-      <h3>
-        <button onClick={() => { setEnabled(!enabled) }}>
-          mouse follower {enabled ? 'off' : 'on'} 
-        </button>
-      </h3>
+      <div style={{
+        position: 'absolute',
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+        border: '1px solid #fff',
+        borderRadius: '50%',
+        opacity: 0.8,
+        pointerEvents: 'none',
+        left: -25,
+        top: -25,
+        width: 50,
+        height: 50,
+        transform: `translate(${position.x}px, ${position.y}px)`
+      }}
+      />
+      <button onClick={() => setEnabled(!enabled)}>
+        {enabled ? 'Desactivar' : 'Activar'} seguir puntero
+      </button>
     </>
   )
 }
